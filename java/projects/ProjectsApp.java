f2fb1168f8a619e13599d00d2df1a773ea7999f3
Khongmong Thao <66309520+rainphantasm@users.noreply.github.com>
2022-09-02 19:12:01 -0500

    Add files via upload

---
+++ java/projects/ProjectsApp.java
@@ -0,0 +1,146 @@
+package projects;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+import projects.entity.Project;
+import projects.exception.DbException;
+import projects.service.ProjectService;
+
+public class ProjectsApp {
+	
+	
+	ProjectService projectService = new ProjectService();
+	
+	// @formatter:off
+	private List<String> operations = List.of(
+			"1) Add a project"
+			);
+	// @formatter:on
+	
+	
+	private Scanner scanner = new Scanner(System.in);
+	
+
+	public static void main(String[] args) {
+		new ProjectsApp().processUserSelections();
+	}
+
+	
+	private void processUserSelections() {
+		boolean done = false;
+		while(!done) {
+			try {
+				int selection = getUserSelection();
+				
+				switch(selection) {
+				
+					case(-1):
+						done =  exitMenu();
+						break;
+						
+					case(1):
+						createProject();
+						break;						
+						
+					default:
+						System.out.println("\n" + selection + " is not a valid selection. Try again.");
+						break;
+				}	
+			}catch (Exception e){
+				System.out.println("\nError: " + e + " Try again.");
+			}
+		}
+		
+	}
+
+
+	private void createProject() {
+		Project project = new Project();
+		String projectName = getStringInput("Enter project name");
+		BigDecimal estimatedHours = getDecimalInput("Enter estimated hours");
+		BigDecimal actualHours = getDecimalInput("Enter actual hours");
+		Integer difficulty = getIntInput("Enter the project difficulty (1-5)");
+		String notes = getStringInput("Enter the project notes");
+		project.setProjectName(projectName);
+		project.setEstimatedHours(estimatedHours);
+		project.setActualHours(actualHours);
+		project.setDifficulty(difficulty);
+		project.setNotes(notes);
+
+		Project dbProject = projectService.addProject(project);
+
+		System.out.println("You successfully created project " + dbProject);
+		
+		
+	}
+
+
+	private BigDecimal getDecimalInput(String prompt) {
+
+		String input = getStringInput(prompt);
+		
+		if(Objects.isNull(input)) {
+			return null;
+		}
+		
+		try {
+			return new BigDecimal(input).setScale(2);
+			
+		}catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid decimal number");
+		}
+	}
+
+
+	private boolean exitMenu() {
+		System.out.println("Exiting the menu.");
+		return true;
+	}
+
+
+	private int getUserSelection() {
+		printOperations();
+		Integer input = getIntInput("Enter a menu selection");
+		return Objects.isNull(input) ? -1 : input;
+	}
+	
+	private void printOperations() {
+		System.out.println("\nThese are the available selections. Press the Enter key to quit:");
+		operations.forEach(line -> System.out.println("   " + line));
+	}
+
+
+	private Integer getIntInput(String prompt) {
+		
+		String input = getStringInput(prompt);
+		
+		if(Objects.isNull(input)) {
+			return null;
+		}
+		
+		try {
+			return Integer.valueOf(input);
+			
+		}catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid number");
+		}		
+	}
+
+
+	private String getStringInput(String prompt) {
+		System.out.printf(prompt + ": ");
+		String input = scanner.nextLine();
+		
+		return input.isBlank() ? null : input.trim();
+	}
+	
+	
+	
+	
+	
+
+
+	
+
+}
